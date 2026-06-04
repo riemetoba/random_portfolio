76ca37f50ee2ad897052b590f18d21dca2f6e8a0
Toba <riemehasan06@gmail.com>
2026-06-04 22:23:37 +0600

    tailwind CLI issue fix

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
-// ============= 
- jQuery(document).ready(function($) {
-            $('.counter').counterUp({
-                delay: 10,
-                time: 1000
-            });
-        });
+// =============
+jQuery(document).ready(function ($) {
+  $(".counter").counterUp({
+    delay: 10,
+    time: 1000,
+  });
+});
+
+    const menuBtn = document.getElementById('menu-btn');
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+  });
